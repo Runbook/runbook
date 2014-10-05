4bc477492ee29452b581462cd1c10591f7ed4a38
Benjamin Cane <ben@bencane.com>
2014-10-05 16:26:31 -0700

    Adding in verification of logentries format

--- crweb/monitorapis/logentries-webhook/__init__.py
+++ crweb/monitorapis/logentries-webhook/__init__.py
@@ -10,19 +10,28 @@ def webCheck(request, monitor, urldata, rdb):
   ''' Process the webbased api call '''
   replydata = { 
     'headers': { 'Content-type' : 'application/json' }
-    }
+  }
   rdata = {}
+
+  ## Ensure data matches a logentries post and not just random requests
   if request.method == "POST" and "payload" in request.form:
     request_verify = True
   else:
     request_verify = False
    
-  ## Monitor
+  ## Verify and then send web check Monitor
   monitor.get(urldata['cid'], rdb)
+  # Verify check_key and api_type matches monitor for cid
   if urldata['check_key'] == monitor.url and urldata['atype'] == monitor.ctype:
+    # Ensure action is failed or healthy
     if urldata['action'] == "failed" or urldata['action'] == "healthy":
+      # Set new status (not saved until monitor.webCheck is performed
       monitor.healthcheck = urldata['action']
-      result = monitor.webCheck(rdb)
+      if request_verify == True:
+        # Send web based health check to dcqueues
+        result = monitor.webCheck(rdb)
+      else:
+        result = False
       if result:
         rdata['result'] = "success"
       else:  
@@ -31,8 +40,6 @@ def webCheck(request, monitor, urldata, rdb):
       rdata['result'] = "failed"
   else:
     rdata['result'] = "invalid key"
-
   replydata['data'] = json.dumps(rdata)
-  
   return replydata
 
