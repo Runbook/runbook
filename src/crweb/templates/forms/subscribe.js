5f9eb6cb4a59fe87759f16e56ff07794d04427c2
Michael Herman <hermanmu@gmail.com>
2014-10-15 18:53:42 -0500

    install guide updates

---
+++ src/crweb/templates/forms/subscribe.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+  $(document).ready(function() {
+    $("#subscribe-cvc").popover({
+        placement : 'auto bottom',
+        container: 'body',
+        trigger: 'click focus',
+    });
+    $("#subscribe-addpacks").popover({
+        placement : 'auto bottom',
+        container: 'body',
+        trigger: 'click focus',
+    });
+
+  });
+
+</script>
