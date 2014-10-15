ad1a62804348ee114941c9a99eb925a73c2b3034
madflojo <ben@bencane.com>
2014-10-14 23:57:46 -0700

    Adding all of the modules and forms and stuff from previous commit that I forgot to add first...

---
+++ crweb/reactionforms/heroku-scale-up/__init__.py
@@ -0,0 +1,25 @@
+######################################################################
+# Cloud Routes Web Application
+# -------------------------------------------------------------------
+# Reaction - Forms Class
+######################################################################
+
+from wtforms import Form
+from wtforms import TextField, PasswordField, SelectField, SelectMultipleField, HiddenField
+from wtforms.validators import DataRequired, ValidationError, Email, Length, Required, URL
+from wtforms.validators import IPAddress, NumberRange, EqualTo
+from ..base import BaseReactForm
+
+class ReactForm(BaseReactForm):
+    ''' Class that creates a Reaction form for the dashboard '''
+
+    apikey = TextField("API Key", validators=[DataRequired(message='API Key is a required field')])
+    appname = TextField("Application Name", validators=[DataRequired(message='Application Name is a required field')])
+    call_on = SelectField("Call On", choices=[('failed', 'Failed Monitors'), ('healthy', 'Healthy Monitors')], validators=[DataRequired(message='Call On is a required field')])
+
+    dyno_type = TextField("Dyno Type", validators=[DataRequired(message='Dyno Type is a required field')])
+    max_size = SelectField("Max Size", choices=[( '1', '1X'), ( '2', '2X'), ( '3', 'PX')], validators=[DataRequired(message='Max Size is a required field')])
+
+
+if __name__ == '__main__':
+    pass
---
+++ static/templates/reactions/heroku-scale-in.js
@@ -0,0 +1,26 @@
+<script type="text/javascript">
+  $(document).ready(function() {
+    $("#heroku-scale-in-apikey").popover({
+        placement : 'auto bottom',
+        container: 'body',
+        trigger: 'click focus',
+    });
+    $("#heroku-scale-in-appname").popover({
+        placement : 'auto bottom',
+        container: 'body',
+        trigger: 'click focus',
+    });
+    $("#heroku-scale-in-dynotype").popover({
+        placement : 'auto bottom',
+        container: 'body',
+        trigger: 'click focus',
+    });
+    $("#heroku-scale-in-minquantity").popover({
+        placement : 'auto bottom',
+        container: 'body',
+        trigger: 'click focus',
+    });
+
+  });
+
+</script>
