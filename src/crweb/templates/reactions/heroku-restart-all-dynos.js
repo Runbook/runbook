ac474b39227f703decb3bd68ff7c005de18f7e5b
madflojo <ben@bencane.com>
2014-10-15 23:11:26 -0700

    bounty 48 initial

---
+++ src/crweb/templates/reactions/heroku-restart-all-dynos.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+  $(document).ready(function() {
+    $("#heroku-restart-dyno-apikey").popover({
+        placement : 'auto bottom',
+        container: 'body',
+        trigger: 'click focus',
+    });
+    $("#heroku-restart-dyno-appname").popover({
+        placement : 'auto bottom',
+        container: 'body',
+        trigger: 'click focus',
+    });
+
+  });
+
+</script>
---
+++ src/crweb/templates/reactions/heroku-restart-dyno.js
@@ -0,0 +1,21 @@
+<script type="text/javascript">
+  $(document).ready(function() {
+    $("#heroku-restart-dyno-apikey").popover({
+        placement : 'auto bottom',
+        container: 'body',
+        trigger: 'click focus',
+    });
+    $("#heroku-restart-dyno-appname").popover({
+        placement : 'auto bottom',
+        container: 'body',
+        trigger: 'click focus',
+    });
+    $("#heroku-restart-dyno-dynoname").popover({
+        placement : 'auto bottom',
+        container: 'body',
+        trigger: 'click focus',
+    });
+
+  });
+
+</script>
