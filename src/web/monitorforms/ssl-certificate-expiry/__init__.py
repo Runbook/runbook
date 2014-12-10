1e6eefeea296b79c3555d8b57431af52ae1f81a4
Sanrag Sood <sanragsood@gmail.com>
2014-12-09 18:36:38 -0800

    Monitor SSL certificate expiration

---
+++ src/web/monitorforms/ssl-certificate-expiry/__init__.py
@@ -0,0 +1,22 @@
+# SSL Certificate expiry check form.
+
+from wtforms import IntegerField,TextField
+from wtforms.validators import DataRequired,NumberRange
+from ..datacenter import DatacenterCheckForm
+
+class CheckForm(DatacenterCheckForm):
+    """Creates a wtforms object for SSL Certificate check monitor."""
+  
+    hostname = TextField(
+        'Host name',
+        validators=[DataRequired(message='Host name is a required field.')])
+    port = IntegerField(
+        'Port number',
+        validators=[NumberRange(min=1,max=65536,message='Port number betweeen 1-65536')])
+    num_days = IntegerField(
+        'Days before cert expiration a warning should be generated.',
+        validators=[NumberRange(min=1,max=365,message='Warning threshold between 1 - 365 days')])
+
+if __name__ == '__main__':
+    pass
+
