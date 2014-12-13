227f317035a838c4690c86eb9daf4ad2cfe2ff5f
Michael Herman <hermanmu@gmail.com>
2014-12-12 17:38:01 -0800

    simplified install/running

--- tmux_3_bridge.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# start bridge and broker in tmux
-cd src/bridge
-tmux new -s 'bridge' -d 'python bridge.py config/config.yml.default'
-tmux split-window -v 'python broker.py config/config.yml.default'
-tmux split-window -v 'python actioner.py config/config.yml.default'
-tmux attach -t 'bridge'
--- tmux_4_actions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# start actioner in tmux
-cd src/actions
-tmux split-window -v 'python broker.py config/config.yml.default'
-tmux split-window -v 'python actioner.py config/config.yml.default'
-tmux attach -t 'actions'
