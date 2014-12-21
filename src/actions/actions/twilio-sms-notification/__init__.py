ba63d3bcc042c3e076fe15c5d0f9057ae0dfe278
Sanrag Sood <sanragsood@gmail.com>
2014-12-21 13:14:54 -0800

    Add Twilio SMS notification reaction

---
+++ src/actions/actions/twilio-sms-notification/__init__.py
@@ -0,0 +1,41 @@
+"""Twilio SMS Notification Reaction"""
+
+import syslog
+import time
+from twilio.rest import TwilioRestClient
+
+
+def __action(**kwargs):
+    redata = kwargs['redata']
+    jdata = kwargs['jdata']
+    run = (
+        jdata['failcount'] >= redata['trigger'] and
+        (time.time() - float(redata['lastrun'])) >= redata['frequency'] and
+        redata['data']['call_on'] in jdata['check']['status']
+    )
+    if run:
+        account_sid = redata['data']['account_sid']
+        auth_token = redata['data']['auth_token']
+        from_address = redata['data']['from_address']
+        to_address = redata['data']['to_address']
+        text = redata['data']['text']
+        return call_twilio(account_sid, auth_token, from_address, to_address, text)
+
+
+def call_twilio(account_sid, auth_token, from_address, to_address, text):
+    client = TwilioRestClient(account_sid, auth_token)
+    message = client.messages.create(to=to_address,from_=from_address,body=text)
+    assert message.status not in ('failed', 'undelivered'), message.ErrorMessage
+    return True
+
+
+def action(**kwargs):
+    try:
+        return __action(**kwargs)
+    except Exception, e:
+        redata = kwargs['redata']
+        syslog.syslog(
+            syslog.LOG_WARNING,
+            'twilio-sms-notification: Reaction {id} failed: {message}'.format(
+                id=redata['id'], message=e.message))
+        return False
