10eacc60c293f0835ada734097f86bd73a84d600
Sanrag Sood <sanragsood@gmail.com>
2014-12-24 11:28:11 -0800

    HTTP GET / POST reaction

---
+++ src/web/reactionforms/utils.py
@@ -0,0 +1,22 @@
+"""Collection of common utility classes and functions."""
+
+from wtforms.validators import ValidationError
+
+
+class HeaderList(object):
+    """Custom wtforms validator for headers."""
+
+    def __call__(self, form, field):
+        try:
+            for header in str.splitlines(str(field.data)):
+                header = header.strip()
+                # Ignore empty lines
+                if not header:
+                    continue
+                key, value = header.split(':')
+                key = key.strip()
+                value = value.strip()
+                assert key
+                assert value
+        except Exception, e:
+            raise ValidationError('Invalid headers. Use key:value format.')
