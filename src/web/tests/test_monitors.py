97d5e99916a501b7aab13fa939c2edacca139dae
Michael Herman <hermanmu@gmail.com>
2014-12-25 07:22:54 -0700

    first monitor test

---
+++ src/web/tests/test_monitors.py
@@ -0,0 +1,34 @@
+######################################################################
+# Runbook Web Application
+# -------------------------------------------------------------------
+# Tests - monitors
+######################################################################
+
+
+import unittest
+
+from flask import url_for, g
+
+from base import BaseTestCase
+from users import User
+
+
+class MonitorTests(BaseTestCase):
+
+    def test_index(self):
+        # Ensure that Flask was set up correctly
+        with self.client:
+            self.client.post(
+                '/login',
+                data=dict(email="test@tester.com", password="password456"),
+                follow_redirects=True
+            )
+            response = self.client.get(
+                '/dashboard/monitors/cr-api',
+                follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('Runbook Webhooks', response.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
