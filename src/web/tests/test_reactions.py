ae28737c47b4334cf0ac59bb429d60e02310746c
Michael Herman <hermanmu@gmail.com>
2014-12-28 09:55:24 -0600

    updated test coverage

--- src/web/tests/test_reactions.py
+++ src/web/tests/test_reactions.py
@@ -112,5 +112,35 @@ class ReactionTests(BaseTestCase):
             self.assertIn('Reaction successfully edited.'.format(
                 new_timestamp), response.data)
 
+    def test_user_can_delete_reaction(self):
+        # Ensure that a logged in user can delete an existing reaction.
+        timestamp = str(datetime.datetime.now())
+        with self.client:
+            self.client.post(
+                '/login',
+                data=dict(email="test@tester.com", password="password456"),
+                follow_redirects=True
+            )
+            self.client.post(
+                '/dashboard/reactions/enotify',
+                data=dict(name=timestamp, trigger=1,
+                          frequency=1, email="test@tester.com",
+                          send_true=True),
+                follow_redirects=True
+            )
+            results = r.table('reactions').filter(
+                {'name': timestamp}).run(g.rdb_conn)
+            for result in results:
+                reaction_id = result['id']
+                break
+            response = self.client.get(
+                '/dashboard/delete-reaction/{0}'.format(reaction_id),
+                follow_redirects=True
+            )
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('Reaction was successfully deleted.',
+                          response.data)
+
+
 if __name__ == '__main__':
     unittest.main()
