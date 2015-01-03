87b09c8a2b8b5d39fcea9cb4e1f94f7cf5d074f6
Michael Herman <hermanmu@gmail.com>
2015-01-03 16:55:17 -0700

    added linode boot

--- src/actions/actions/linode-boot/__init__.py
+++ src/actions/actions/linode-boot/__init__.py
@@ -38,7 +38,7 @@ def call_linode(redata, jdata, logger):
     ''' Perform actual call '''
     url = 'https://api.linode.com/'
     params = {
-        "api_action": "linode.boot",
+        "api_action": "linode.reboot",
         "LinodeID": int(redata['data']['linode_id']),
         "api_key": str(redata['data']['api_key'])
     }
@@ -49,12 +49,12 @@ def call_linode(redata, jdata, logger):
     except:
         return False
     if req.status_code >= 200 and req.status_code < 300:
-        line = 'linode-boot: Request to {0} sent for monitor {1} - \
+        line = 'linode-reboot: Request to {0} sent for monitor {1} - \
                 Successful'.format(url, jdata['cid'])
         logger.info(line)
         return True
     else:
-        line = 'linode-boot: Request to {0} sent for monitor {1} - \
+        line = 'linode-reboot: Request to {0} sent for monitor {1} - \
                 False'.format(url, jdata['cid'])
         logger.info(line)
         return False
