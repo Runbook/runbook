a12c61d5e86f10d0f0b310c9204d56d2defd8f8d
Dustin Chapman <dustin.r.chapman@gmail.com>
2015-01-03 06:02:09 -0500

    Add logging to http-get-statuscode monitor for docs example

--- src/monitors/checks/http-get-statuscode/__init__.py
+++ src/monitors/checks/http-get-statuscode/__init__.py
@@ -17,16 +17,26 @@ import requests
 def check(**kwargs):
     """ Perform a http get request and validate the return code """
     jdata = kwargs['jdata']
+    logger = kwargs['logger']
     headers = {'host': jdata['data']['host']}
     timeout = 3.00
     url = jdata['data']['url']
     try:
         result = requests.get(
             url, timeout=timeout, headers=headers, verify=False)
-    except:
+    except Exception as e:
+        line = 'http-get-statuscode: Reqeust to {0} sent for monitor {1} - ' \
+               'had an exception: {2}'.format(url, jdata['cid'], e)
+        logger.error(line)
         return False
     rcode = str(result.status_code)
     if rcode in jdata['data']['codes']:
+        line = 'http-get-statuscode: Reqeust to {0} sent for monitor {1} - ' \
+               'Successful'.format(url, jdata['cid'])
+        logger.info(line)
         return True
     else:
+        line = 'http-get-statuscode: Reqeust to {0} sent for monitor {1} - ' \
+               'Failure'.format(url, jdata['cid'])
+        logger.info(line)
         return False
