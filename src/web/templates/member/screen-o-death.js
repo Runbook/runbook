7225f124fdbfbc7ecc8af1d12f0b11ab35c145a6
Sanrag Sood <sanragsood@gmail.com>
2015-01-12 20:39:06 -0800

    Ask user to confirm before deleting a monitor or reaction

--- src/web/templates/member/screen-o-death.js
+++ src/web/templates/member/screen-o-death.js
@@ -1,6 +1,17 @@
 <script type="text/javascript">
   $(document).ready(function() {
    $('a[data-toggle="tab"]:first').trigger("shown.bs.tab");
-
+   $('.btn-delete-monitor').click(function(e){
+       var monitorName = $(e.target).attr('data');
+       if (!window.confirm('Are you sure you want to delete monitor '+ monitorName + '?')) {
+           e.preventDefault();
+       }
+   });
+   $('.btn-delete-reaction').click(function(e){
+       var reactionName = $(e.target).attr('data');
+       if (!window.confirm('Are you sure you want to delete reaction ' + reactionName + '?')) {
+           e.preventDefault();
+       }
+   });
   });
 </script>
