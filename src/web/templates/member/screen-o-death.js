1f912ac2d029c3ba1392ded7ea7ad58bddb8bf87
Sanrag Sood <sanragsood@gmail.com>
2015-01-18 00:57:57 -0800

    Use bootbox to provide confirmation message in a modal dialog

--- src/web/templates/member/screen-o-death.js
+++ src/web/templates/member/screen-o-death.js
@@ -2,16 +2,24 @@
   $(document).ready(function() {
    $('a[data-toggle="tab"]:first').trigger("shown.bs.tab");
    $('.btn-delete-monitor').click(function(e){
+       e.preventDefault();
        var monitorName = $(e.target).attr('data');
-       if (!window.confirm('Are you sure you want to delete monitor '+ monitorName + '?')) {
-           e.preventDefault();
-       }
+       var url = $(e.target).attr('href');
+       bootbox.confirm('Are you sure you want to delete monitor '+ monitorName + '?', function(result) {
+           if (result) {
+               window.location.href = url;
+           }
+       });
    });
    $('.btn-delete-reaction').click(function(e){
+       e.preventDefault();
        var reactionName = $(e.target).attr('data');
-       if (!window.confirm('Are you sure you want to delete reaction ' + reactionName + '?')) {
-           e.preventDefault();
-       }
+       var url = $(e.target).attr('href');
+       bootbox.confirm('Are you sure you want to delete reaction ' + reactionName + '?', function(result){
+           if (result) {
+               window.location.href = url;
+           }
+       });
    });
   });
 </script>
