b79736afb8615bb63c725b2cbd581eed21e6310f
Michael Herman <hermanmu@gmail.com>
2015-06-07 08:48:27 -0600

    updated readme

--- src/web/tests.py
+++ src/web/tests.py
@@ -1,4 +1,14 @@
 import unittest
 
-tests = unittest.TestLoader().discover('tests')
-unittest.TextTestRunner(verbosity=2, buffer=True).run(tests)
+
+def test():
+    """Runs the unit tests without coverage."""
+    tests = unittest.TestLoader().discover('tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    else:
+        return 1
+
+if __name__ == '__main__':
+    test()
