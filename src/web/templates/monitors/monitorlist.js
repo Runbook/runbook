b0ff72f13db7c18c99a6e37ce6918741241510f0
Benjamin Cane <ben@bencane.com>
2015-10-04 21:22:46 +0000

    Cleaned up some things

--- src/web/templates/monitors/monitorlist.js
+++ src/web/templates/monitors/monitorlist.js
@@ -1,7 +1,7 @@
 <script type="text/javascript">
 var options = {
     valueNames: [ 'name', 'category', 'description' ],
-    page: 6,
+    page: 10,
     plugins: [
       ListPagination({})
     ]
