7e448d0637d0a78aaeb67507c8ae2efef1349f79
Benjamin Cane <ben@bencane.com>
2015-10-18 01:57:36 +0000

    Cleaning up monitor forms

--- src/web/monitorforms/base.py
+++ src/web/monitorforms/base.py
@@ -11,6 +11,9 @@ from wtforms.validators import DataRequired
 
 class BaseCheckForm(Form):
 
+    def __init__(self, *args, **kwargs):                                        
+        super(BaseCheckForm,self).__init__(*args, **kwargs)                          
+
     ''' Class that creates an TCP Check form for the dashboard '''
     name = TextField(
         "Name",
@@ -33,7 +36,15 @@ class BaseCheckForm(Form):
         'domain' : 'example.com',
         'apikey' : 'API Key',
         'threshold' : '10',
+        'hostname' : 'example.com',
+        'host' : 'example.com',
+        'url' : 'https://example.com/server-health',
+        'extra_headers' : 'header:value',
+        'port' : '443',
     }
+
+    webhook_include = "monitors/webhooks/general.html"
+
 
 if __name__ == '__main__':  # pragma: no cover
     pass                    # pragma: no cover
