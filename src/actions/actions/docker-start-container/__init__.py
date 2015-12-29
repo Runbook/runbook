b77be27a4e178f4c48c9560029a1ae7396e53999
Benjamin Cane <ben@bencane.com>
2015-12-29 05:59:22 +0000

    Adding Docker reactions
    * Kill
    * Pull
    * Restart
    * RM
    * Start

---
+++ src/actions/actions/docker-start-container/__init__.py
@@ -0,0 +1,46 @@
+from fabric.api import env, run, hide
+from ..utils import ShouldRun
+
+def __action(**kwargs):
+    redata = kwargs['redata']
+    jdata = kwargs['jdata']
+    if ShouldRun(redata, jdata):
+        env.gateway = redata['data']['gateway']
+        env.host_string = redata['data']['host_string']
+        env.user = redata['data']['username']
+        env.key = redata['data']['sshkey']
+        env.disable_known_hosts = True
+        env.warn_only = True
+        env.abort_on_prompts = True
+        env.shell = "/bin/sh -c"
+        cmd = ""
+        if redata['data']['use_sudo'] == "true":
+            cmd = "sudo "
+        cmd = cmd + "docker run -d {0} {1}".format(redata['data']['add_options'],
+                                                   redata['data']['image'])
+        try:
+            results = run_cmd(cmd)
+            if results.succeeded:
+                return True
+            else:
+                raise Exception(
+                    'Command Execution Failed: {0} - {1}'.format(results.return_code, results))
+        except:
+            raise Exception(
+                'Command failed to execute')
+
+def run_cmd(cmd):
+    with hide('output', 'running', 'warnings'):
+        return run(cmd, timeout=1200)
+
+
+def action(**kwargs):
+    try:
+        return __action(**kwargs)
+    except Exception, e:  #pylint: disable=broad-except
+        redata = kwargs['redata']
+        logger = kwargs['logger']
+        logger.warning(
+            'docker-start-container: Reaction {id} failed: {message}'.format(
+                id=redata['id'], message=e.message))
+        return False
